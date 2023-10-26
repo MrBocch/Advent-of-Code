34d0de5a090241837a058a819db32be79b875746
MrBochh <jorgealberto1436@gmail.com>
2023-10-25 22:33:38 -0600

    ruby solution for day1 y 2022

---
+++ 2022/day1/day1.rb
@@ -0,0 +1,37 @@
+def part1
+  max_sum = 0
+  bucket = []
+  File.open("input.txt", "r").each do |line|
+    line.chomp!
+
+    if line != "" 
+      bucket.push line.to_i
+    else 
+      max_sum = [max_sum, bucket.sum].max
+      bucket = []
+    end
+  end
+
+  return max_sum
+end
+
+def part2
+  sum = 0
+  bucket = []
+  File.open("input.txt", "r").each do |line|
+    line.chomp!
+
+    if line != "" 
+      sum += line.to_i
+    else 
+      bucket.push sum
+      sum = 0
+    end
+  end
+
+  return bucket.max(3).sum
+end
+
+
+puts "Part1: #{part1}"
+puts "Part2: #{part2}"
