4b514f9e27f9536711049e4c3d6ad04e68dc4dba
MrBochh <jorgealberto1436@gmail.com>
2023-11-03 22:21:56 -0600

    2022 day 6 part 2

--- 2022/day6/day6.rb
+++ 2022/day6/day6.rb
@@ -9,4 +9,18 @@ def part1
   return nil
 end
 
+def part2 
+  f = IO.read("input.txt")
+  (14...f.length).each do |i|
+    # ugly
+    s = Set[f[i-14], f[i-13], f[i-12], f[i-11],
+            f[i-10], f[i-9], f[i-8], f[i-7],
+            f[i-6], f[i-5], f[i-4], f[i-3],
+            f[i-2], f[i-1]]
+    return i if s.length == 14
+  end
+
+  return nil
+end
 puts "Part1: #{part1}"
+puts "Part2: #{part2}"
