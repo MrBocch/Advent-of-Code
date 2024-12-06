999af5d17f7a9b590190e76573510df6fe7b9164
MrBocch <jorgealberto1436@gmail.com>
2024-12-06 17:32:13 -0600

    pt2, bruteforce, i dont know why this does not work, but it seems to be really close, its the answer for someother input, cant just run it agaain because it took SOO long for it to completelol, what if i try it in crystal

--- 2024/day6/day6.rb
+++ 2024/day6/day6.rb
@@ -63,4 +63,119 @@ def find_pointer(grid)
   return pntr
 end
 
+def part2
+  grid = File.open("input.txt").read.split("\n")
+  ptr = find_pointer(grid)
+  loops = 0
+  while true do
+    if ptr.direction == :up
+      if ptr.y-1 < 0 then break end
+
+      if grid[ptr.y-1][ptr.x] == "#"
+        ptr.direction = :right
+      else
+        pclone = ptr.clone
+        # change it back, rather not use a deep clone
+        grid[ptr.y-1][ptr.x] = "#"
+        loops += 1 if a_loop? grid, pclone
+        grid[ptr.y-1][ptr.x] = "."
+
+        ptr.y -= 1
+      end
+
+    elsif ptr.direction == :right
+      if ptr.x+1 >= grid[0].length then break end
+
+      if grid[ptr.y][ptr.x+1] == "#"
+        ptr.direction = :down
+      else
+        pclone = ptr.clone
+        grid[ptr.y][ptr.x+1] = "#"
+        loops += 1 if a_loop? grid, pclone
+        grid[ptr.y][ptr.x+1] = "."
+
+        ptr.x += 1
+      end
+
+    elsif ptr.direction == :down
+      if ptr.y+1 >= grid.length then break end
+
+      if grid[ptr.y+1][ptr.x] == "#"
+        ptr.direction = :left
+      else
+        pclone = ptr.clone
+        grid[ptr.y+1][ptr.x] = "#"
+        loops += 1 if a_loop? grid, pclone
+        grid[ptr.y+1][ptr.x] = "."
+
+        ptr.y += 1
+      end
+
+    elsif ptr.direction == :left
+      if ptr.x-1 < 0 then break end
+
+      if grid[ptr.y][ptr.x-1] == "#"
+        ptr.direction = :up
+      else
+        pclone = ptr.clone
+        grid[ptr.y][ptr.x-1] = "#"
+        loops += 1 if a_loop? grid, pclone
+        grid[ptr.y][ptr.x-1] = "."
+
+        ptr.x -= 1
+      end
+    end
+  end
+
+  return loops
+end
+
+def a_loop?(grid, ptr)
+  visited = []
+  while true do
+    if visited.include? [ptr.x, ptr.y, ptr.direction] then return true end
+
+    visited << [ptr.x, ptr.y, ptr.direction]
+
+    if ptr.direction == :up
+      if ptr.y-1 < 0 then break end
+
+      if grid[ptr.y-1][ptr.x] == "#"
+        ptr.direction = :right
+      else
+        ptr.y -= 1
+      end
+
+    elsif ptr.direction == :right
+      if ptr.x+1 >= grid[0].length then break end
+
+      if grid[ptr.y][ptr.x+1] == "#"
+        ptr.direction = :down
+      else
+        ptr.x += 1
+      end
+
+    elsif ptr.direction == :down
+      if ptr.y+1 >= grid.length then break end
+
+      if grid[ptr.y+1][ptr.x] == "#"
+        ptr.direction = :left
+      else
+        ptr.y += 1
+      end
+
+    elsif ptr.direction == :left
+      if ptr.x-1 < 0 then break end
+
+      if grid[ptr.y][ptr.x-1] == "#"
+        ptr.direction = :up
+      else
+        ptr.x -= 1
+      end
+    end
+  end
+  return false
+end
+
 puts "part1: #{part1}"
+puts "part2: #{part2}"
