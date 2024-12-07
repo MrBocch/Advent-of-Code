2ab9c1dd23a4223a28e4823d48113963067c8162
MrBocch <jorgealberto1436@gmail.com>
2024-12-07 15:10:56 -0600

    solving this part was just as easy, when i realised you only had to make up a new operator, i think haskell is just well suited for these problems, also, there is alot of copy pasting and repeating, let me try to refactor it just so its prettier

--- 2024/day7/day7.hs
+++ 2024/day7/day7.hs
@@ -16,6 +16,10 @@ main = do
     let p1_ans = sum (map fst valid)
     putStrLn ("part1: " ++ show p1_ans)
 
+    let valid = filter solver2 processed
+    let p2_ans = sum (map fst valid)
+    putStrLn ("part2: " ++ show p2_ans)
+
 
 solver tuple = solver' (fst tuple) (head $ snd tuple) (tail $ snd tuple)
 
@@ -26,7 +30,22 @@ solver' target carry (x:[]) = (solver' target (carry + x) []) ||
 solver' target carry (x:xs) = if carry > target then False else
                 (solver' target (carry + x) xs) ||
                 (solver' target (carry * x) xs)
---
+
+--part2, same function, now you just add the pipe op, with concats to numbers
+solver2 tuple = solver2' (fst tuple) (head $ snd tuple) (tail $ snd tuple)
+
+solver2' target carry [] = if target == carry then True else False
+solver2' target carry (x:[]) = (solver2' target ((+) carry x) []) ||
+                              (solver2' target ((*) carry x) []) ||
+                              (solver2' target ((pipeOp) carry x) [])
+
+solver2' target carry (x:xs) = if carry > target then False else
+                (solver2' target ((+) carry x) xs) ||
+                (solver2' target ((*) carry x) xs) ||
+                (solver2' target ((pipeOp) carry x) xs)
+
+pipeOp n1 n2 = read (show n1 ++ show n2) :: Int
+
 processString :: String -> (Int, [Int])
 processString input =
     let (firstPart, rest) = span (/= ':') input
