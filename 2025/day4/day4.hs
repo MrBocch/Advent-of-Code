df59cfb277dd19da91840ce3fffd8a150008cc8b
MrBocch <jorgealberto1436@gmail.com>
2026-01-31 23:42:02 -0600

    i can't tell whats wrong with this code

--- 2025/day4/day4.hs
+++ 2025/day4/day4.hs
@@ -8,17 +8,16 @@ parseLine "" = []
 
 parseInput :: [String] -> [[Int]]
 parseInput = map parseLine 
-
-get :: Int -> [a] -> Maybe a 
-get i l 
-  | i < 0 || i >= length l = Nothing
-  | otherwise              = Just (l !! i)
-
+    
 part1 :: [[Int]]  -> Int 
 part1 m = forLoop 0 0 0 
   where
-    rows = head m
-    cols = head . head $ m 
+    rows           = length m
+    cols           = (length . head) m  
+    get :: Int -> [a] -> Maybe a 
+    get i l 
+      | i < 0 || i >= length l = Nothing
+      | otherwise              = Just (l !! i)
     -- I was struggeling for ever, until decided to ask chatgpt how to compose the get function
     -- with itself
     -- It hit me like a truck when I saw the type definition for (>=>)
@@ -27,8 +26,6 @@ part1 m = forLoop 0 0 0
     -- Wow, now I see, >=> is a lot more general. Should reread that chapter 
     get' i j = (get i >=> get j) m
 
-    -- i want to map this function over
-    -- the matrix but idk how, some variation of zipWithIndex
     countNeighbors i j = sum . fmap sum $ 
                          [ get' (i-1) (j-1)
                          , get' (i-1) (j+0)
@@ -39,15 +36,15 @@ part1 m = forLoop 0 0 0
                          , get' (i+1) (j-1)
                          , get' (i+0) (j-1)
                          ]
-    - 
-    forLoop :: Int -> Int -> Int 
+      
     forLoop i j acc 
-      | i >= rows = acc   
+      | i >= rows = acc  
       | j >= cols = forLoop (i+1) 0 acc 
-      | otherwise = forLoop i (j+1) acc 
-    
+      | otherwise = forLoop i (j+1) (acc + (if countNeighbors i j < 3 then 1 else 0))
+ 
+
 main :: IO ()
 main = do
-  content <- readFile "t.txt"
+  content <- readFile "input.txt"
   let parsed = parseInput . words $ content 
   print (part1 parsed) 
