2f285c2c4a4b19a3fb84b5dcf002a0d8093eff74
MrBocch <jorgealberto1436@gmail.com>
2026-01-31 23:38:45 -0600

    really dissapointed with my self, the code is wrong, now I changed it and it wont compile worst part is i dont know why, this should be trivial wtf am i doing?

--- 2025/day4/day4.hs
+++ 2025/day4/day4.hs
@@ -1,28 +1,53 @@
--- Dont want to fully get into it because im busy
--- but I'll write down the function signatures
--- have a basic idea of what im going to do.
--- I could probably just reuse the code from when
--- implemented game of life in C.
+import Control.Monad ((>=>))
+
+parseLine :: String -> [Int]
+parseLine (x:xs) 
+  | x == '@' = 1 : parseLine xs
+  | x == '.' = 0 : parseLine xs
+parseLine "" = []
 
 parseInput :: [String] -> [[Int]]
--- turn ".@", if . -> 0
---            if @ -> 1
+parseInput = map parseLine 
 
-countNeighbors :: [[Int]] -> (Int, Int) -> Int  
--- takes matrix, x,y, returns the count of neighbors
+get :: Int -> [a] -> Maybe a 
+get i l 
+  | i < 0 || i >= length l = Nothing
+  | otherwise              = Just (l !! i)
 
-surroundingHelper :: (Int, Int) -> [(Int, Int)] 
--- lets say for cell at 0,0
--- check for
--- [ ( 0,  1)
---   ( 0, -1)
---   ( 1,  0)
---   (-1,  0)
---   ( 1,  1)
---   ( 1, -1)
---   (-1,  1)
---   (-1, -1)
--- ]
--- do i just manually write it out?
--- don't know how to automate it with code.
--- after im done should look for examples of game of life.
+part1 :: [[Int]]  -> Int 
+part1 m = forLoop 0 0 0 
+  where
+    rows = head m
+    cols = head . head $ m 
+    -- I was struggeling for ever, until decided to ask chatgpt how to compose the get function
+    -- with itself
+    -- It hit me like a truck when I saw the type definition for (>=>)
+    -- when I read the Kleisli chapter in Category Theory for Programmers
+    -- I thought it was only useful for composing functions that return logs
+    -- Wow, now I see, >=> is a lot more general. Should reread that chapter 
+    get' i j = (get i >=> get j) m
+
+    -- i want to map this function over
+    -- the matrix but idk how, some variation of zipWithIndex
+    countNeighbors i j = sum . fmap sum $ 
+                         [ get' (i-1) (j-1)
+                         , get' (i-1) (j+0)
+                         , get' (i-1) (j+1)
+                         , get' (i+0) (j+1)
+                         , get' (i+1) (j+1)
+                         , get' (i+1) (j+0)
+                         , get' (i+1) (j-1)
+                         , get' (i+0) (j-1)
+                         ]
+    - 
+    forLoop :: Int -> Int -> Int 
+    forLoop i j acc 
+      | i >= rows = acc   
+      | j >= cols = forLoop (i+1) 0 acc 
+      | otherwise = forLoop i (j+1) acc 
+    
+main :: IO ()
+main = do
+  content <- readFile "t.txt"
+  let parsed = parseInput . words $ content 
+  print (part1 parsed) 
