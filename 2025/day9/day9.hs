efcf66d29861e6715d596dd03ad9efc6dc7825d7
MrBocch <jorgealberto1436@gmail.com>
2026-04-12 23:45:46 -0600

    got to use a fmap, cool

--- 2025/day9/day9.hs
+++ 2025/day9/day9.hs
@@ -1,20 +1,10 @@
-{-
-splitOn :: String -> Char -> (String, String)
-splitOn s c = splitOn' s ""
-    where
-        splitOn' :: String -> String -> (String, String)
-        splitOn' (x:xs) acc
-          | x == c    = (reverse acc, xs)
-          | otherwise = splitOn' xs (x:acc)
-
-Idk why chatgpt now decides to tell me about break (== c) or span
-One would think, if you ask for the equivalent of ruby's s.split(c)
-but no.
--}
+import Data.Bifunctor
+-- split s p :: String -> Char -> (String, String)
+split c s = (\(a,b) -> (a, tail b)) . break (== c) $ s
 
 main :: IO ()
 main = do
   contents <- readFile "input.txt"
-  let proc = map ((\(a,b) -> (read a :: Int, read (tail b) :: Int)) . break (== ','))
-             . lines $ contents
+  -- let proc = map (bimap (read :: String -> Int) (read :: String -> Int) . split ',') . lines $ contents
+  let proc = map (bimap read read . split ',') . lines $ contents :: [(Int, Int)]
   print proc
